136f9e7f84f31d3bce8f3bb86faeb594b4905e33
ritikajangra <ritikajangra1920@gmail.com>
2025-05-20 20:31:36 +0530

    Add all files

--- FaceDetection/settings.py
+++ FaceDetection/settings.py
@@ -121,3 +121,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+import os
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
